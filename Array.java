9254ccaf41ae8507f4de568ebe1037c6ad3f679c
Nathashakb <nathashakb123@gmil.com>
2024-03-17 20:46:41 +0530

    initial commit

---
+++ Array.java
@@ -0,0 +1,12 @@
+package QA;
+
+public class Array {
+    public static void main(String[] args) {
+        String names[] = new String[20];
+        names[0] = "Nathasha";
+        names[1] = "Hiran";
+        names[2] = "Pawya";
+
+        System.out.println(names[2]);
+    }
+}
