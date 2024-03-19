5445ca762bac8823f37a3f7c23a5bb1e4a952f64
Nathashakb <nathashakb123@gmil.com>
2024-03-19 22:40:03 +0530

    add array 3

--- Array.java
+++ Array.java
@@ -2,11 +2,11 @@ package QA;
 
 public class Array {
     public static void main(String[] args) {
-        String names[] = new String[20];
+        String names[] = new String[15];
         names[0] = "Nathasha";
         names[1] = "Hiran";
         names[2] = "Pawya";
-
+        names[3] = "Pathmila";
 
         System.out.println(names[1]);
     }
